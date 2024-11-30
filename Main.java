93e5444590e52212cdfcbf4e5d68b06aeca6ed0f
Katie <159663445+kchuhak@users.noreply.github.com>
2024-11-30 18:12:57 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // loop to print the message five times
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Welcome to Java");
+        }
+    }
+}
